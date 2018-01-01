b1a45b1782a9d67bb39c9e4fc5f1cb515620896d
hcely <yepeijie1900@126.com>
2018-01-01 14:55:29 +0800

    update

--- zr-common/src/main/common/zr/unit/DynamicDubboPortProtocol.java
+++ zr-common/src/main/common/zr/unit/DynamicDubboPortProtocol.java
@@ -1,11 +1,8 @@
-package zr.bean;
-
-import org.springframework.stereotype.Component;
+package zr.unit;
 
 import com.alibaba.dubbo.common.utils.NetUtils;
 import com.alibaba.dubbo.config.ProtocolConfig;
 
-@Component
 public class DynamicDubboPortProtocol extends ProtocolConfig {
 	private static final long serialVersionUID = 1L;
 
